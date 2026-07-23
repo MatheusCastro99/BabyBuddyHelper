a6609002558a8a3934e4b7acbbe701a7fb65a053
Matheus Castro <mpcastro0525@gmail.com>
2026-07-23 19:04:44 -0400

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BabyPrepRegistry
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Pages/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace BabyPrepRegistry
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void onCheckListClicked(object? sender, EventArgs e)
+        {
+
+        }
+
+        private void onRegistryClicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
