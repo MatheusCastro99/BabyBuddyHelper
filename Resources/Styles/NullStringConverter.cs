886573dcd892b11d50b3902c6779f297ac6da154
Matheus Castro <mpcastro0525@gmail.com>
2026-08-13 19:27:38 -0400

    - Added a separator on ChecklistPage that separates description from date, and priority from location
    - ChecklistPage tasks now also shows Date and Location of Appointments

---
+++ Resources/Styles/NullStringConverter.cs
@@ -0,0 +1,22 @@
+﻿using BabyBuddyHelper.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace BabyBuddyHelper.Resources.Styles
+{
+    internal class IsAppointmentModelConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            return value is AppointmentModel;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
