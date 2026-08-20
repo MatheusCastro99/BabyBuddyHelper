c83245c6079792e407d51df21e737735deffe06b
Matheus Castro <mpcastro0525@gmail.com>
2026-08-20 14:30:30 -0400

    - Substituted SyncFusion Calendar for SyncFusion Scheduler. The latter makes more sense with what I want to accomplish with the calendar view and appointments

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Syncfusion.Maui.Core.Hosting;
 
 namespace BabyBuddyHelper
 {
@@ -13,7 +14,8 @@ namespace BabyBuddyHelper
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+                })
+                .ConfigureSyncfusionCore();
 
 #if DEBUG
     		builder.Logging.AddDebug();
--- Pages/CalendarPage.xaml.cs
+++ Pages/CalendarPage.xaml.cs
@@ -1,7 +1,11 @@
+using Syncfusion.Maui.Scheduler;
+
+
 namespace BabyBuddyHelper.Pages;
 
 public partial class CalendarPage : ContentPage
 {
+	private DateTime? SelectedDate { get; set; } = DateTime.Today;
 	public CalendarPage()
 	{
 		InitializeComponent();
