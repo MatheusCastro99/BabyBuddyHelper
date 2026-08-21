1112f551bb99f891155132b4ed11c67937c5663d
Matheus Castro <mpcastro0525@gmail.com>
2026-08-21 09:26:44 -0400

    - App modified to properly fetch and load SyncFusion License from a .env file using DotNetEnv package

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using DotNetEnv;
+using Microsoft.Extensions.Logging;
 using Syncfusion.Maui.Core.Hosting;
+using Syncfusion.Licensing;
+using System.Reflection;
+using System.Diagnostics;
 
 namespace BabyBuddyHelper
 {
@@ -8,6 +12,19 @@ namespace BabyBuddyHelper
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
+
+            //Get the current assembly to access the embedded .env file
+            var assembly = Assembly.GetExecutingAssembly();
+
+            //Since it is dealing with secrets, using statement is optimal to ensure the stream is properly disposed of after use
+            using var stream = assembly.GetManifestResourceStream("BabyBuddyHelper..env"); //Format: {DefaultNamespace}.{Folder(if not root)}.{FileName}
+
+            if (stream != null) //attempts to load the .env file and register keys
+            {
+                Env.Load(stream);
+                Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(Environment.GetEnvironmentVariable("SYNCFUSION_LICENSE"));
+            }
+
             builder
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
@@ -20,6 +37,9 @@ namespace BabyBuddyHelper
 #if DEBUG
     		builder.Logging.AddDebug();
 #endif
+
+            Debug.WriteLine(Environment.GetEnvironmentVariable("SYNCFUSION_LICENSE"));
+
 
             return builder.Build();
         }
