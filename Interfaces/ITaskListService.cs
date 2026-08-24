78fc908f4b7f3729a31ee766a8bb836d2bd31286
Matheus Castro <mpcastro0525@gmail.com>
2026-08-24 13:17:50 -0400

    - Added ITaskListService Interface for better encapsulation - Added Class TaskListService for better modularity and maintenability - Goal is to detach TaskList from ChecklistPage so it can be used in other parts of the program as well

---
+++ Interfaces/ITaskListService.cs
@@ -0,0 +1,19 @@
+﻿using BabyBuddyHelper.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace BabyBuddyHelper.Interfaces
+{
+    internal interface ITaskListService //Setting interface for TaskListService to implement. This allows for dependency injection, easier testing,
+                                        // and better encapsulation.
+    {
+        ObservableCollection<TaskModel> Tasks { get; }
+        void Add(TaskModel task);
+        void Remove(TaskModel task);
+        void Update(TaskModel task);
+        void OrganizeByPriority();
+        void OrganizeByPending();
+    }
+}
---
+++ Services/TaskListService.cs
@@ -0,0 +1,64 @@
+﻿using BabyBuddyHelper.Interfaces;
+using BabyBuddyHelper.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace BabyBuddyHelper.Services
+{
+    internal class TaskListService : ITaskListService //Implemente Interface ITaskListService to provide functionality for managing a list of tasks.
+    {                                                 //This class will be used to add, remove, update, and organize tasks in the application.
+        public ObservableCollection<TaskModel> Tasks { get; } = new();
+
+        public void Add(TaskModel task)
+        {
+            Tasks.Add(task);
+        }
+
+        public void Remove(TaskModel task)
+        {
+            Tasks.Remove(task);
+        }
+
+        public void Update(TaskModel updatedTask)
+        {
+            var existingTask = Tasks.FirstOrDefault(x => x.Id == updatedTask.Id);
+
+            if (existingTask is null)
+            return;
+
+            var index = Tasks.IndexOf(existingTask);
+            Tasks[index] = updatedTask;
+        }
+
+        public void OrganizeByPriority()
+        {
+            var sortedList = Tasks
+            .OrderByDescending(x => x.TaskPriority)
+            .ToList();
+
+            RebuildCollection(sortedList);
+        }
+
+        public void OrganizeByPending()
+        {
+            var sortedList = Tasks
+            .OrderBy(x => x.IsCompleted)
+            .ThenByDescending(x => x.TaskPriority)
+            .ToList();
+
+            RebuildCollection(sortedList);
+        }
+
+        private void RebuildCollection(List<TaskModel> sortedTasks)
+        {
+            Tasks.Clear();
+
+            foreach (var task in sortedTasks)
+            {
+                Tasks.Add(task);
+            }
+        }
+    }
+}
