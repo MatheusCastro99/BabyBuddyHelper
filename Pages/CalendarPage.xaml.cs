a2054f7345f5dc440e3e3515ca2531f80b73267c
Matheus Castro <mpcastro0525@gmail.com>
2026-08-25 12:48:38 -0400

    - Calendar now refreshes as tasks are added / edited / deleted

--- Pages/CalendarPage.xaml.cs
+++ Pages/CalendarPage.xaml.cs
@@ -13,18 +13,30 @@ public partial class CalendarPage : ContentPage
 {
 	public DateTime? SelectedDate { get; set; } = DateTime.Today;
     private readonly ITaskListService _taskListService; //Dependency Injection for TaskListService
-    public ObservableCollection<AppointmentModel> TaskList { get; private set; }//Will hold instances of TaskModel and AppointmentModel
+    public ObservableCollection<AppointmentModel> TaskList { get; private set; }//Will hold instances of AppointmentModel
 
     public CalendarPage(ITaskListService taskListService)
 	{
 		InitializeComponent();
 
         _taskListService = taskListService;
-        TaskList = _taskListService.GetAppointments().ToObservableCollection();
+        _taskListService.Tasks.CollectionChanged += (s, e) => //Subscribe to the CollectionChanged event of the TaskListService's Tasks collection
+        {
+            CalendarRefresh();
+        };
+
+        CalendarRefresh();
+
+        BindingContext = this;
+    }
+
+    private void CalendarRefresh()
+    {
+        TaskList = _taskListService.GetAppointments().ToObservableCollection(); //Update TaskList with the latest appointments
 
         var schedulerAppointments = new ObservableCollection<SchedulerAppointment>(
-            _taskListService.GetAppointments()              //Filter instances of tasks in _taskListService that are AppointmentModels
-            .Select(appt => new SchedulerAppointment        //Then, for each appt filtered, creates a SchedulerAppointment counterpart
+            _taskListService.GetAppointments()                        //Filter instances of tasks in _taskListService that are AppointmentModels
+            .Select(appt => new SchedulerAppointment                    //Then, for each appt filtered, creates a SchedulerAppointment counterpart
             {
                 Subject = appt.TaskName,
                 StartTime = appt.SchedulerStartTime,
@@ -33,13 +45,8 @@ public partial class CalendarPage : ContentPage
             }));
 
         Calendar.AppointmentsSource = schedulerAppointments; //Actual Binding for sfScheduler
-
-        BindingContext = this;
     }
 
     //Implement add and edit functionaly for appointments through tapped event, triggering modal AddTaskPage
     //with prefilled data for editing, or empty data (exept by date) for adding a new appointment.
-
-    //Implement refresh functionality to keep appointments in sync with the TaskListService every time the list is modified.
-    //This can be done by subscribing to the CollectionChanged event of the TaskListService's Tasks collection and updating the Calendar.AppointmentsSource accordingly.
 }
